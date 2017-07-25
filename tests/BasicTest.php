2549c5b3d9622cf99c6a64d67d7d095a99dffe33
BePsvPT <freedom@csie.io>
2017-07-25 17:06:02 +0800

    Update tests

---
+++ tests/BasicTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Bepsvpt\CSPBuilder\CSPBuilder;
+
+class BasicTest extends PHPUnit_Framework_TestCase
+{
+    public function test_csp()
+    {
+        $data = json_decode(file_get_contents(__DIR__.'/vectors/basic-csp.json'), true);
+
+        $csp = new CSPBuilder($data);
+
+        $this->assertEquals(
+            trim(file_get_contents(__DIR__.'/vectors/basic-csp.out')),
+            $csp->getHeaderArray()['Content-Security-Policy']
+        );
+    }
+
+    public function test_upgrade_insecure_beats_disable_https_conversion_flag()
+    {
+        $data = json_decode(file_get_contents(__DIR__.'/vectors/basic-csp.json'), true);
+
+        $data['form-action']['allow'][0] = 'http://example.com';
+
+        $csp = new CSPBuilder($data);
+
+        $csp->disableHttpTransformOnHttpsConnection();
+
+        $header = $csp->getHeaderArray()['Content-Security-Policy'];
+
+        $this->assertContains('https://example.com', $header);
+        $this->assertNotContains('http://example.com', $header);
+    }
+}
